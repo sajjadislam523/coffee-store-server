67ddb57ecd15dd72333a004cd532765e80e1c553
sajjadislam523 <sajjad.islam523@gmail.com>
2024-12-01 02:00:44 +0600

    changed the middleware setting

--- index.js
+++ index.js
@@ -8,7 +8,10 @@ const port = process.env.PORT || 5000;
 
 
 // middleware
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+}));
 app.use(express.json());
 
 
