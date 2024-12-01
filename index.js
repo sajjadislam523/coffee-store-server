8c6649e3dd510f0dc1922d9c6036ed036b467846
sajjadislam523 <sajjad.islam523@gmail.com>
2024-12-01 15:58:25 +0600

    updated configuration in the backend

--- index.js
+++ index.js
@@ -7,11 +7,14 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 
+const corsOptions = {
+    "origin": "*",
+    "methods": "GET, HEAD, PUT, PATCH, POST, DELETE",
+
+}
+
 // middleware
-app.use(cors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-}));
+app.use(cors(corsOptions));
 app.use(express.json());
 
 
@@ -26,7 +29,9 @@ app.listen(port, () => {
 // basic setup done!
 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.owq8r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.owq8r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+const uri = process.env.MONGODB_URI;
 
 
 
